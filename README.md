52ad4aa223c6442c34724f489ec01e15f8820696
Connor Cowan <ccowan003@comcast.net>
2023-04-13 17:17:16 -0700

    First Build Complete / functioning

---
+++ README.md
@@ -0,0 +1,3 @@
+# TODO App
+
+Using TypeScript & ReactJS
---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
+import { Todo } from "./todo.model";
+
+const App: React.FunctionComponent = () => {
+  //We want to render our todos in State, so when we update our todos we will re render App component
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const todoAddHandler = (text: string) => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
+  };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos((preTodos) => {
+      return preTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+  return (
+    <div className="App">
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
+    </div>
+  );
+};
+
+export default App;
